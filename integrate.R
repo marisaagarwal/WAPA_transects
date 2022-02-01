f07d20be85daea5e452bd07135dc2ef08c85b36e
Marisa Agarwal <marisaagarwal@Marisas-MacBook-Pro.local>
2022-02-01 13:54:23 +1000

    intital commit

---
+++ integrate.R
@@ -0,0 +1,82 @@
+##  Project Name:  WAPA Internship Coral Transect Analyses
+##
+##  Objective:     Assess how coral & benthic community composition changes
+##                 over space and time.
+##
+##  Approach:      1. Analyze coral transect photographs from Adelup,
+##                    Asan, & Agat using CoralNet
+##                 2. Analyze data to fulfill research objective
+##
+##  Authors:       Marisa Agarwal
+##
+##  Start Date:    2022-02-01
+##
+
+##  Notes:         RUN THIS FILE BEFORE EVERY SESSION 
+
+
+
+##  1. Set up core functionality
+
+    # clean up
+    rm(list=ls())
+      
+    # call to core packages for data manipulation
+    library(dplyr)
+    library(tidyr)
+    library(magrittr)      
+    library(purrr)
+    library(lubridate)
+    library(stringr)
+    library(forcats)      
+    library(tidyverse)
+    library(see)
+    library(broom)
+
+    # for importing different formats
+    library(readr)
+    library(readxl)
+    
+    # for easier tidy stats 
+    library(rstatix)
+    library(easystats)
+    library(multcomp)
+    library(vegan)
+    
+    # call to visualisation & output generation
+    library(ggplot2)
+    library(GGally)
+    library(Cairo)
+    library(extrafont)
+    library(RColorBrewer)
+    library(viridis)
+    library(ggdist)
+    library(ggsci)
+    library(plotrix)
+    library(multcompView)
+    library(ggpubr)
+
+
+    # point to working directory
+    setwd("research/WAPA_transects")
+
+    # # set font for graphical outputs
+    # theme_set(theme_bw(base_family = "Helvetica"))
+    # CairoFonts(  # slight mod to example in ?CairoFonts page
+    # regular    = "Helvetica:style = Regular",
+    # bold       = "Helvetica:style = Bold",
+    # italic     = "Helvetica:style = Oblique",
+    # bolditalic = "Helvetica:style = BoldOblique"
+    # )
+    
+    # # call to map theme
+    # source("R/theme_nothing.R")
+
+
+## 2. Generate core data objects
+## if things need to be loaded in before every session, include them here
+    
+    
+    
+    
+
